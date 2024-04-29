ad6f6d8c4a6e3ec2fa62523599c8ee8243677e6e
fiorotto8 <davide.fiorina@gssi.it>
2024-04-29 14:07:56 +0200

    new cuts in BaseScript

--- IP.py
+++ IP.py
@@ -1,6 +1,12 @@
 import uproot
 import numpy as np
 import ROOT
+import argparse
+
+parser = argparse.ArgumentParser(description="Split a ROOT TTree into multiple subsets.")
+parser.add_argument("input_file", help="root file with the directionality analyzed data")
+parser.add_argument("output_file", help="Name of the output file")
+args = parser.parse_args()
 
 def create_fill_TH2(name,x_name,y_name,z_name, x_vals, y_vals, weights=None, x_bins=20, y_bins=20,write=True):
     # Convert x_bins and y_bins to integers explicitly
@@ -24,13 +30,20 @@ def create_fill_TH2(name,x_name,y_name,z_name, x_vals, y_vals, weights=None, x_b
     return hist
 
 # Open the original ROOT file
-with uproot.open("Tracks/Analysis_Ar8020.root") as file:
+with uproot.open(args.input_file) as file:
     # Access the TTree
     tree = file["track_info"]
     # Read the data from the branch you're interested in
     X_IP = tree["X_ImpactPoint"].array(library="np")
     Y_IP = tree["Y_ImpactPoint"].array(library="np")
 
-main = ROOT.TFile("IPdistr_Ar8020.root", "RECREATE")
+main = ROOT.TFile(args.output_file, "RECREATE")
 
-IP=create_fill_TH2("IP positions","X","Y","Entries",X_IP,Y_IP,x_bins=20,y_bins=20)
+# Check for NaN values
+nan_indices_X = np.isnan(X_IP)
+nan_indices_Y = np.isnan(Y_IP)
+# Filter out NaN values
+X_IP_filtered = X_IP[~nan_indices_X]
+Y_IP_filtered = Y_IP[~nan_indices_Y]
+
+IP=create_fill_TH2("IP positions","X","Y","Entries",X_IP_filtered,Y_IP_filtered,x_bins=20,y_bins=20)
